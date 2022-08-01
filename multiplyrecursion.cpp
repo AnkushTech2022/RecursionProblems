03ee263ddfe0d447b2b6e9c4ddf7b1aa635351c9
Ankush raj <104245302+AnkushTech2022@users.noreply.github.com>
2022-08-01 15:26:46 +0530

    Update multiplyrecursion.cpp

--- multiplyrecursion.cpp
+++ multiplyrecursion.cpp
@@ -1,1 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int multiplyRecursion(int arr[]){
+  int length=sizeof(arr)/sizeof(0);
+  if(length<0){
+    return;
+  }
+  else{
+    return multplyRecursion(arr*arr[length-1]);
+  }
+}
+
+
+int main(){
+  int arr[100];
+  int n;
+  cout<<"Enter size of array:"<<endl;
+  cin>>n;
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+  
+ cout<<multiplyRecursion(int arr);
+}
