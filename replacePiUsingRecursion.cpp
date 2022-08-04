58d88d2f30b9658168f310f0b099b99c1cd44dd8
Ankush raj <104245302+AnkushTech2022@users.noreply.github.com>
2022-08-04 16:48:40 +0530

    Update replacePiUsingRecursion.cpp

--- replacePiUsingRecursion.cpp
+++ replacePiUsingRecursion.cpp
@@ -1,1 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+void replacePiHelper(char str[], int start){
+  if(str[start]=='\0' || str[start+1]=='\0'){
+  return;
+  }
+  if(str[start]=='p' || str[start+1]=='i'){
+  for(int i=strlen(str);i>=start+2;i--){
+  str[i+2]=str[i];
+  }
+    str[start]='3';
+    str[start+1]='.';
+    str[start+2]='1';
+    str[start+3]='4';
+  }
 
+}
+
+void replacePi(char str[]){
+replacePiHelper(str, 0);
+}
+
+int main(){
+  char str[]=pipipipipppiiii;
+  
+  replacePi(str);
+  cout<<str;
+  
+  return 0;
+}
+
+
+
+//O/P:   3.143.143.143.14pp3.14iii;
