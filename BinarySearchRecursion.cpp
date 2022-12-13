79066b92099e461ffd27dd074584be2d5b4e807f
Ankush raj <104245302+AnkushTech2022@users.noreply.github.com>
2022-12-13 18:28:13 +0530

    Update BinarySearchRecursion.cpp

--- BinarySearchRecursion.cpp
+++ BinarySearchRecursion.cpp
@@ -1,2 +1,21 @@
 
 // Binary search problem using recursion in c++
+
+#include<bits/stdc++.h>
+using namespace std;
+void binarySearch(){
+  
+
+}
+int main(){
+  int size;
+  cout<<"Enter size of array:";
+  cin>>size;
+  int input;
+  vector<int>array;
+  for(int i=0;i<size;i++){
+    cin>>input;
+    array.emplace_back(input);
+  }
+
+}
